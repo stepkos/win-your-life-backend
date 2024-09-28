eae1f08a72f41e33494af0f5b03e479c63869731
AliakseiSamoshyn(272 <aliakseisamoshyn@gmail.com>
2024-09-28 19:39:25 +0200

    small change

--- backend/modules/users/models.py
+++ backend/modules/users/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
+from modules.core.models import BaseModel
+
 
 # Create your models here.
 
 
-class UserProfile(models.Model):
-    id = models.UUIDField(primary_key = True)
+class UserProfile(BaseModel):
     is_dark_mode = models.BooleanField(default = False)
     is_private = models.BooleanField(default = True)
 
 
-class User(models.Model):
-    id = models.UUIDField(primary_key = True)
+class User(BaseModel):
     email = models.EmailField(unique = True)
     user_profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
     friends = models.ManyToManyField("self", blank = True, related_name="friends")
