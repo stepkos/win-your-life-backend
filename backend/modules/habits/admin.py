fc9cbfb10051163dbb66456bd1e894123fc7a66b
stepkos <stepkos@wp.pl>
2024-09-28 21:44:34 +0200

    feat: Implement admin panel for Habits

--- backend/modules/habits/admin.py
+++ backend/modules/habits/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from modules.habits.models import Habit, Day, TaskForDay
 
-# Register your models here.
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = "user", "content", "updated_at"
+
+
+@admin.register(Day)
+class DayAdmin(admin.ModelAdmin):
+    list_display = "user", "date", "updated_at"
+
+
+@admin.register(TaskForDay)
+class TaskForDayAdmin(admin.ModelAdmin):
+    list_display = "day", "is_done", "content", "updated_at"
+    list_filter = "is_done", "day"
+    search_fields = "content", "day"
