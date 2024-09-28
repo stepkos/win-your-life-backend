72f3a43e751330f8f94ac5d41e36cd24ec4d3a18
AliakseiSamoshyn(272 <aliakseisamoshyn@gmail.com>
2024-09-28 13:06:06 +0200

    initial commit

---
+++ djangofett/djangofettapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
