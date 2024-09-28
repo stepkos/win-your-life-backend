c2f7a0c4ffc0567c00a5e01648da9d36ab2fa08a
stepkos <stepkos@wp.pl>
2024-09-28 21:24:19 +0200

    feat: Implement HabitViewSet and CurrentDay view

--- backend/modules/habits/urls.py
+++ backend/modules/habits/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from modules.habits.views import HabitViewSet
+from modules.habits.views import HabitViewSet, CurrentDay
 
 router = DefaultRouter()
 router.register(r'habits', HabitViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('current_day/', CurrentDay.as_view())
 ]
--- backend/modules/habits/views.py
+++ backend/modules/habits/views.py
@@ -1,10 +1,34 @@
+from django.utils import timezone
 from rest_framework import viewsets
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from modules.habits.models import Habit
-from modules.habits.serializers import HabitSerializer
+from modules.habits.models import Habit, Day, TaskForDay
+from modules.habits.serializers import HabitSerializer, DaySerializer, TaskForDaySerializer
 
 
 class HabitViewSet(viewsets.ModelViewSet):
-    queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+    # permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Habit.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class CurrentDay(APIView):
+    # permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+
+        current_day = Day.objects.filter(
+            user=request.user,
+            date=timezone.now().date()
+        )
+
+        instance = DaySerializer(current_day)
+        return Response(instance.data)
