b55fac2c53aebbf6f897b41e468aa0908bff0584
stepkos <stepkos@wp.pl>
2024-09-29 00:01:41 +0200

    refactor: Better endpoint paths

--- backend/modules/users/urls.py
+++ backend/modules/users/urls.py
@@ -3,9 +3,9 @@ from modules.users import views
 
 
 urlpatterns = [
-    path('api/user/friend', views.UserFriendsView.as_view()),
-    path('api/user/profile', views.UserProfileView.as_view()),
-    path('api/user', views.UserView.as_view()),
-    path('api/user/info', views.UserInfoView.as_view()),
-    path('api/user/streak', views.UserStreakView.as_view()),
+    path('user/friend/', views.UserFriendsView.as_view()),
+    path('user/profile/', views.UserProfileView.as_view()),
+    path('user/', views.UserView.as_view()),
+    path('user/info/', views.UserInfoView.as_view()),
+    path('user/streak/', views.UserStreakView.as_view()),
 ]
