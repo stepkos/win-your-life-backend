4653b955e97e99ee6c775876204b2a6cb5307fbc
stepkos <stepkos@wp.pl>
2024-09-28 22:45:24 +0200

    feat: Implement part of email service

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'drf_spectacular',
     'modules.authentication',
     'modules.core',
+    'modules.emails',
     'modules.habits',
     'modules.users',
 ]
---
+++ backend/modules/emails/services.py
@@ -0,0 +1,36 @@
+import smtplib
+from email.message import EmailMessage
+from typing import Sequence
+
+
+def create_message(from_: str, to: str | Sequence[str], subject: str, body: str) -> EmailMessage:
+    message = EmailMessage()
+    message['From'] = from_
+    message['To'] = to
+    message['Subject'] = subject
+    message.set_content(body)
+    return message
+
+
+def send_email(
+    message: EmailMessage, login: str, password: str, receiver: str | Sequence[str]
+) -> bool:
+    try:
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(login, password)
+            smtp.sendmail(login, receiver, message.as_string())
+    except Exception as e:
+        return False
+    return True
+
+
+class EmailService:
+    DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+    def __init__(self):
+        self.login = config.EMAIL_USER
+        self.password = config.EMAIL_APP_CLIENT_ACCESS_CODE
+
+    def send_email_to_yourself(self, subject: str, body: str) -> bool:
+        message = create_message(self.login, self.login, subject, body)
+        return send_email(message, self.login, self.password, self.login)
