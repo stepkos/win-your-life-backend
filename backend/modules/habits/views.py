64b6b602ef33376f74f6a158df881ff2d89d891a
stepkos <stepkos@wp.pl>
2024-09-28 18:56:49 +0200

    feat: Implement HabitViewSet

--- backend/modules/habits/views.py
+++ backend/modules/habits/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+from modules.habits.models import Habit
+from modules.habits.serializers import HabitSerializer
+
+
+class HabitViewSet(viewsets.ModelViewSet):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
