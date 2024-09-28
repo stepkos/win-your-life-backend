f77c7fb0388d4519ab2cca8ea22a4b7f8f5b92f7
AliakseiSamoshyn(272 <aliakseisamoshyn@gmail.com>
2024-09-28 22:37:19 +0200

    extend user info

--- backend/modules/users/models.py
+++ backend/modules/users/models.py
@@ -10,11 +10,13 @@ class UserProfile(BaseModel):
     is_dark_mode = models.BooleanField(default = False)
     is_private = models.BooleanField(default = True)
 
+class UserInfo(BaseModel):
+    first_name = models.CharField(max_length = 100)
+    last_name = models.CharField(max_length = 100)
+    streak = models.IntegerField(default = 0)
 
 class User(BaseModel):
-
     email = models.EmailField(unique = True)
     user_profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    user_info = models.OneToOneField(UserInfo, on_delete=models.CASCADE)
     friends = models.ManyToManyField("self", blank = True, related_name="friends")
-
-
