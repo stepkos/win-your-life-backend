e3391ff991e5710611d99409c74e8d1e8de967d5
stepkos <stepkos@wp.pl>
2024-09-28 15:50:04 +0200

    feat: Start core app, implement BaseModel

---
+++ backend/modules/core/models.py
@@ -0,0 +1,15 @@
+import uuid
+
+from django.db import models
+
+
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id})"
