5ff957ff30f2df093e57800472c6a21cabb9eb39
AliakseiSamoshyn(272 <aliakseisamoshyn@gmail.com>
2024-09-28 13:11:25 +0200

    added basic client models

--- djangofett/djangofettapp/models.py
+++ djangofett/djangofettapp/models.py
@@ -3,11 +3,17 @@ from django.db import models
 # Create your models here.
 
 
-class User(models.Model):
+class Client(models.Model):
     id = models.UUIDField(primary_key=True, editable=False)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
     email = models.EmailField()
     password = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class ClientInfo(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False)
+    client = models.OneToOneField(Client, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
